aac163987dda58016fae29d212c3d02488fa0063
Sen Jiang <senj@google.com>
2019-01-09 16:51:18 -0800

    Remove utils::SetRootTempDir().
    This function was originally added to customize temp dir in recovery,
    but we are no longer using any temp file during update, so this is not
    needed anymore.
    
    Test: mma
    Change-Id: I7bfdad4307e4ce7cf3bdbc547e9a2faf1379e50b

--- sideload_main.cc
+++ sideload_main.cc
@@ -43,11 +43,6 @@ using std::vector;
 using update_engine::UpdateEngineStatus;
 using update_engine::UpdateStatus;
 
-namespace {
-// The root directory used for temporary files in update_engine_sideload.
-const char kSideloadRootTempDir[] = "/tmp/update_engine_sideload";
-}  // namespace
-
 namespace chromeos_update_engine {
 namespace {
 
@@ -223,10 +218,6 @@ int main(int argc, char** argv) {
 
   // xz-embedded requires to initialize its CRC-32 table once on startup.
   xz_crc32_init();
-
-  // When called from recovery, /data is not accessible, so we need to use
-  // /tmp for temporary files.
-  chromeos_update_engine::utils::SetRootTempDir(kSideloadRootTempDir);
 
   vector<string> headers = base::SplitString(
       FLAGS_headers, "\n", base::KEEP_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
